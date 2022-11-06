76fe76a4c6ebbe5841424d902ab73c7f23f503f1
Fabian <100328289+FabianUlisesCarrillo@users.noreply.github.com>
2022-11-06 11:10:27 -0800

    Added tasks to localstorage

--- app.js
+++ app.js
@@ -2,31 +2,27 @@
 const listsContainer = document.querySelector('.task-list');
 const newListForm = document.querySelector('[data-new-list-form]');
 const newListInput = document.querySelector('[data-new-list-input]');
+const deleteListBtn = document.querySelector('[data-delete-list-btn]');
+// Variables
+const LOCAL_STORAGE_LIST_KEY = 'task.lists';
+const LOCAL_STORAGE_SELECTED_LIST_ID_KET = 'task.selectedListId';
+let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [];
+let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KET);
 // Function to create a todo
 // const createTodo = () => {
     
 // }
-// sample data
-let lists = [
-    {
-        id: 0,
-        name: 't'
-    },
-    {
-        id: 1,
-        name: 'nae'
-    },
-    {
-        id: 2,
-        name: 'tdo'
-    }
-];
 // Function to clear container
 const clearElement = (element) => {
     while(element.firstChild) {
         element.removeChild(element.firstChild);
     };
 };
+// Save list to localstorage
+const save = () => {
+    localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists));
+    localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KET, selectedListId);
+}
 // Function to render todos
 const render = () => {
     clearElement(listsContainer);
@@ -35,12 +31,19 @@ const render = () => {
     listElement.dataset.listId = list.id;
     listElement.classList.add('list-name');
     listElement.innerText = list.name;
+    if(list.id === selectedListId) {
+        listElement.classList.add('active-list');
+    }
     listsContainer.appendChild(listElement);
     });
 };
 const createList = (name) => {
     return { id: Date.now().toString(), name: name, tasks: [] }
-}
+};
+const saveAndRender = () => {
+    save();
+    render();
+};
 render();
 // Function to get toods from local storage
 // const getLocalTodos = () => {
@@ -59,5 +62,16 @@ newListForm.addEventListener('submit', (e) => {
     const list = createList(listName);
     newListInput.value = null;
     lists.push(list);
-    render();
+    saveAndRender();
+});
+listsContainer.addEventListener('click', (e) => {
+    if(e.target.tagName.toLowerCase() === 'li') {
+        selectedListId = e.target.dataset.listId;
+        saveAndRender();
+    }
+});
+deleteListBtn.addEventListener('click', (e) => {
+    lists = lists.filter(list => list.id !== selectedListId)
+    selectedListId = null;
+    saveAndRender();
 });
